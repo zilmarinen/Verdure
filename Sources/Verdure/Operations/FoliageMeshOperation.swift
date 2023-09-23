72ed4dc803ec3c2666cbd816d679a2bedb4f91d7
Zack Brown <zack@zackbrown.co.uk>
2023-09-23 11:37:05 +0100

    Add foliage mesh operation

---
+++ Sources/Verdure/Operations/FoliageMeshOperation.swift
@@ -0,0 +1,42 @@
+//
+//  FoliageMeshOperation.swift
+//
+//  Created by Zack Brown on 21/09/2023.
+//
+
+import Bivouac
+import Euclid
+import Foundation
+import PeakOperation
+
+public class FoliageMeshOperation: ConcurrentOperation,
+                                   ProducesResult {
+    
+    public var output: Result<Mesh, Error> = Result { throw ResultError.noResult }
+    
+    private let foliageType: FoliageType
+    
+    public init(foliageType: FoliageType) {
+        
+        self.foliageType = foliageType
+    }
+    
+    public override func execute() {
+        
+        do {
+            
+            let origin = foliageType.area.center(at: .tile)
+            
+            let trunk = try foliageType.render(trunk: origin - foliageType.trunk.center(at: .tile))
+            let canopy = try foliageType.render(canopy: (origin - foliageType.canopy.center(at: .tile)) + foliageType.trunkApex)
+
+            output = .success(trunk.union(canopy))
+        }
+        catch {
+
+            output = .failure(error)
+        }
+
+        finish()
+    }
+}
